2eac285eeb93ec52af57f9d50d5fd4890635bf7b
Shreyarp12 <shreyarp121@gmail.com>
2026-08-27 08:22:12 +0530

    Added auto-update for pin_hash column

--- database/db.py
+++ database/db.py
@@ -7,4 +7,15 @@ def get_db_connection():
     conn = sqlite3.connect(Config.DATABASE_PATH)
     conn.row_factory = sqlite3.Row
     conn.execute("PRAGMA foreign_keys = ON;")
+    
+    # --- AUTO-UPDATE SCHEMA HACK ---
+    # Try to add the pin_hash column. If it already exists, SQLite will 
+    # throw an OperationalError, which we can safely ignore!
+    try:
+        conn.execute('ALTER TABLE users ADD COLUMN pin_hash TEXT')
+        conn.commit()
+    except sqlite3.OperationalError:
+        pass # Column already exists, do nothing
+    # -------------------------------
+    
     return conn
